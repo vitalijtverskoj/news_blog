981b67c04ac78aed01f54c636b88db199a227f94
vitalijtverskoj <vitalijtverskoj@gmail.com>
2023-01-10 23:06:35 +0300

    add package user

--- blog/app.py
+++ blog/app.py
@@ -1,4 +1,7 @@
-from flask import Flask
+from time import time
+
+from flask import Flask, g
+from flask import request
 
 app = Flask(__name__)
 
@@ -8,6 +11,53 @@ def index():
     return 'Hello!'
 
 
-@app.route('/greet/<name>/')
-def greet_name(name: str):
-    return f'Hello {name}!'
+@app.route('/greet/<int:name>/')
+def greet_name(name: int):
+    return f'Hello, {isinstance(name, int)}!'
+
+
+@app.route("/user/")
+def read_user():
+    name = request.args.get("name")
+    surname = request.args.get("surname")
+    return f"User {name or '[no name]'} {surname or '[no surname]'}"
+
+
+@app.route("/status/", methods=["GET", "POST"])
+def custom_status_code():
+    if request.method == "GET":
+        return """\
+        To get response with custom status code
+        send request using POST method
+        and pass `code` in JSON body / FormData
+        """
+    print("raw bytes data:", request.data)
+    if request.form and "code" in request.form:
+        return "code from form", request.form["code"]
+    if request.json and "code" in request.json:
+        return "code from json", request.json["code"]
+    return "", 204
+
+
+@app.before_request
+def process_before_request():
+    """
+    Sets start_time to `g` object
+    """
+    g.start_time = time()
+
+
+@app.after_request
+def process_after_request(response):
+    """
+    adds process time in headers
+    """
+    if hasattr(g, "start_time"):
+        response.headers["process-time"] = time() - g.start_time
+    return response
+
+
+@app.errorhandler(404)
+def handler_404(error):
+    app.logger.error(error)
+    return '404'
