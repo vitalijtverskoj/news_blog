893c900bfae2dd847c9d67fab411e254a753abd9
Vitalijtverskoj <vitalijtverskoj@gmail.com>
2023-02-19 19:49:23 +0300

    add api

--- blog/api/article.py
+++ blog/api/article.py
@@ -1,3 +1,4 @@
+from combojsonapi.event.resource import EventsResource
 from flask_combo_jsonapi import ResourceList, ResourceDetail
 
 from blog.extensions import db
@@ -5,7 +6,21 @@ from blog.models import Article
 from blog.schemas import ArticleSchema
 
 
+class ArticleListEvent(EventsResource):
+
+    def event_get_count(self):
+        return {'count': Article.query.count()}
+
+
+class ArticleDetailEvent(EventsResource):
+
+    def event_get_count_by_author(self, **kwargs):
+        return {'count': Article.query.filter(Article.author_id == kwargs['id']).count()}
+
+
+
 class ArticleList(ResourceList):
+    events = ArticleListEvent
     schema = ArticleSchema
     data_layer = {
         'session': db.session,
@@ -14,6 +29,7 @@ class ArticleList(ResourceList):
 
 
 class ArticleDetail(ResourceDetail):
+    events = ArticleDetailEvent
     schema = ArticleSchema
     data_layer = {
         'session': db.session,
